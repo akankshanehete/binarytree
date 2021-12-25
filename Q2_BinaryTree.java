f1b8e9488f2728e55e4fad51e4afe7cccd32fbd7
Akanksha Nehete <83188708+akankshanehete@users.noreply.github.com>
2021-12-25 17:45:03 -0500

    Update Q2_BinaryTree.java

--- Q2_BinaryTree.java
+++ Q2_BinaryTree.java
@@ -1,20 +1,9 @@
-/* Assignment 8 (100 marks in total; 5% of the final score of this course)
- *
- * Question 2 (60 marks)
-    Use Java to implement a binary tree and implement the following classes and methods.
+/* 
     * 1. Design the class for the binary tree
     * 2. addChild: adds a child to a node if the node does not have two children already.
     * 3. deleteChild: delete a child from a node if the child is a leaf node.
     * 4. In the main() method, use your code to build a tree A in the following example.
     * 5. Delete the node with key=20 in tree A, to get the Tree B in the following example.
-
-* Example:
-           50                            50
-        /     \         delete(20)      /   \
-      30      70       --------->    30     70
-     /  \    /  \                     \    /  \
-   20   40  60   80                   40  60   80
-         (tree A)                       (Tree B)
 
  */
 
@@ -35,7 +24,7 @@ class Q2_BinaryTree {
     Node root; // The root node of the binary tree.
 
     // Design the constructor of the class 'Q2_BinaryTree'
-    // (5 marks)
+
     Q2_BinaryTree (int item) {
         /* place your code here */
         root = new Node(item);
@@ -46,9 +35,9 @@ class Q2_BinaryTree {
     // if 'is_left' is true, then add the 'child_node' as a left child of the 'parent node'
     // if 'is_left' is false, then add the 'child_node' as a right child of the 'parent node'
     // if the target position of the child is already taken, then we should stop adding.
-    // (20 marks)
+   
     public void addChild(Node parent_node, Node child_node, boolean is_left){
-        /* place your code here */
+       
 
         if(is_left == true && parent_node.left == null){
             parent_node.left = child_node;
@@ -64,9 +53,9 @@ class Q2_BinaryTree {
     // if 'is_left' is true, then delete the left child of the 'parent_node'
     // if 'is_left' is false, then delete the right child of the 'parent_node'
     // if the target child to delete is null, then the deletion should stop.
-    // (20 marks)
+  
     public void deleteChild(Node parent_node, boolean is_left) {
-        /* place your code here */
+       
 
         if(is_left == true && parent_node.left != null){
             parent_node.left = null;
@@ -77,13 +66,13 @@ class Q2_BinaryTree {
         }
     }
 
-    // This method is for TA's use.
+    
     private void printTree() {
         String prefix = "";
         printTreeCore(prefix, this.root, false);
     }
 
-    // This method is for TA's use.
+   
     private void printTreeCore(String prefix, Node n, boolean isLeft) {
         if (n != null) {
             System.out.println (prefix + (isLeft ? "L-- " : "R-- ") + n.key);
@@ -93,7 +82,7 @@ class Q2_BinaryTree {
     }
 
     public static void main(String[] args) {
-        /* place your code here to build Tree A (10 marks) */
+        /* building tree A
         Q2_BinaryTree binarytreeA = new Q2_BinaryTree(50);
         Node n30 = new Node (30);
         Node n70 = new Node (70);
@@ -109,7 +98,7 @@ class Q2_BinaryTree {
         binarytreeA.addChild(n70, n80,false);
 
 
-        /* place your code here to convert Tree A into Tree B (5 marks) */
+        /* converting tree A into Tree B*/
         Q2_BinaryTree binarytreeB = new Q2_BinaryTree(50);
         binarytreeB = binarytreeA;
         binarytreeB.deleteChild(n30,true);
